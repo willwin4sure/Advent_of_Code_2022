0e5c97429cdcc7f2485c96fc2d229a82e597e240
William <williamy330@gmail.com>
2022-12-01 20:41:19 -0500

    day 1

---
+++ day1.py
@@ -0,0 +1,12 @@
+with open("input1.txt", "r") as f:
+    data = f.read().splitlines()
+    s = 0
+    ss = []
+    for d in data:
+        if d == "":
+            ss.append(s)
+            s = 0
+        else:
+            s += int(d)
+    ss.sort()
+    print(ss[-1] + ss[-2] + ss[-3])
