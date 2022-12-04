959cc2cad8fe1b471e10f47070a4d8f0796c88a4
William <williamy330@gmail.com>
2022-12-04 00:11:29 -0500

    day 4

---
+++ day4/day4b.py
@@ -0,0 +1,17 @@
+with open("input4.txt", "r") as f:
+    data = f.read().splitlines()
+
+    s = 0
+
+    for line in data:
+        ranges = line.split(',')
+        nums = []
+        for r in ranges:
+            nums.extend(r.split('-'))
+
+        nums = list(map(int, nums))
+
+        if max(nums[0], nums[2]) <= min(nums[1], nums[3]):
+            s += 1
+
+    print(s)
